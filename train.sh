5b7d75dd346360972fc855497f2ae241a26c7c4a
Anil Thomas <anlthms@users.noreply.github.com>
2021-09-10 18:27:15 -0700

    Add trial.sh

--- train.sh
+++ train.sh
@@ -15,4 +15,4 @@
 
 export TF_CPP_MIN_LOG_LEVEL=2
 
-python3 ../code/main.py --epochs 10 -b 16 --seed 0
+python3 ../code/main.py --epochs 10 -b 8 --seed 0
---
+++ trial.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+python3 ../code/main.py --epochs 3 -b 8 --seed 0
